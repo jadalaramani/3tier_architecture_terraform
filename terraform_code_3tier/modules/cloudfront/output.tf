88e36a0d5b27e3a57227b6b4a7f92264b03a4b90
Jadala Ramani <jadalaramani@gmail.com>
2025-02-28 09:05:36 +0530

    Delete terraform_code_3tier/modules/cloudfront directory

--- terraform_code_3tier/modules/cloudfront/output.tf
+++
@@ -1,9 +0,0 @@
-output "cloudfront_distribution_id" {
-  description = "CloudFront Distribution ID"
-  value       = aws_cloudfront_distribution.frontend_distribution.id
-}
-
-output "cloudfront_distribution_domain_name" {
-  description = "CloudFront Distribution Domain Name"
-  value       = aws_cloudfront_distribution.frontend_distribution.domain_name
-}
