e1ca99405f585f787828aa1b48099d3e3b211e7a
jadalaramani <jadalaramani@gmail.com>
2025-02-28 16:50:25 +0530

    update the code

--- terraform_code_3tier/modules/launch_templates/backend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt update -y
-sudo pm2 startup
-sudo env PATH=$PATH:/usr/bin /usr/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
-sudo systemctl start pm2-root
-sudo systemctl enable pm2-root 
